59cfb0ebb44741aaf1d383758409f8c245372350
Shankarsimu <shaknarnarayan791@gmail.com>
2026-09-09 23:14:53 +0530

    updste

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+ 
 export default defineConfig({
+  //added from google result
+  timeout: isCi ? 60000 : 30000, // Give CI double the time (60 seconds)
+  expect: {
+    timeout: isCi ? 10000 : 5000,
+  },
+  workers: isCi ? 2 : undefined,   // Restrict workers on CI to prevent thrashing
+  retries: isCi ? 2 : 0, 
+  
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -37,43 +34,9 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    }
+ 
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+   
 });
